3915c716e5c077a32a7f01f201d9ef0f68810f4e
AmanTrance <aman.hobo@gmail.com>
2024-10-09 01:26:24 +0530

    complete user authorization

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -7,14 +7,48 @@ package graph
 import (
 	"context"
 	"errors"
-	"fmt"
 	"slogger/database"
 	"slogger/graph/model"
+
+	"github.com/go-pg/pg/v10"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // Createuser is the resolver for the createuser field.
 func (r *mutationResolver) Createuser(ctx context.Context, username string, email string, password string) (*model.CreateUserResponse, error) {
-	panic(fmt.Errorf("not implemented: Createuser - createuser"))
+	connection := database.GetContext(ctx)
+	var user model.User
+	_, err := connection.DBState.QueryOne(&user, `SELECT * from users WHERE username = (?) OR email = (?);`, &username, &email)
+	if err != nil {
+		if err == pg.ErrMultiRows {
+			response := model.CreateUserResponse{
+				Success: false,
+				Message: "Username or email already exists!",
+			}
+			return &response, err
+		} else if err == pg.ErrNoRows {
+			hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), 12)
+			_, err := connection.DBState.Exec(`INSERT INTO users (username, email, password, token) VALUES(?, ?, ?, ?);`, &username, &email, string(hashedPassword), nil)
+			if err != nil {
+				response := model.CreateUserResponse{
+					Success: false,
+					Message: "Database is down right now!",
+				}
+				return &response, err
+			} else {
+				response := model.CreateUserResponse{
+					Success: true,
+					Message: "User created!",
+				}
+				return &response, nil
+			}
+		}
+	}
+	response := model.CreateUserResponse{
+		Success: false,
+		Message: "User already exists!",
+	}
+	return &response, nil
 }
 
 // Getuser is the resolver for the getuser field.
@@ -33,7 +67,32 @@ func (r *queryResolver) Getuser(ctx context.Context, token *string) (*model.User
 
 // Gettoken is the resolver for the gettoken field.
 func (r *queryResolver) Gettoken(ctx context.Context, username *string, email *string, password string) (*model.GetTokenResponse, error) {
-	panic(fmt.Errorf("not implemented: Gettoken - gettoken"))
+	if username == nil || email == nil {
+		return nil, errors.New("Username and email should not be empty!")
+	}
+	connection := database.GetContext(ctx)
+	var user model.User
+	_, err := connection.DBState.QueryOne(&user, `SELECT * from users WHERE username = (?) AND email = (?)`, *username, *email)
+	if err != nil {
+		response := model.GetTokenResponse{
+			Success: false,
+			Token:   nil,
+		}
+		return &response, err
+	}
+	passwordError := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if passwordError != nil {
+		response := model.GetTokenResponse{
+			Success: false,
+			Token:   nil,
+		}
+		return &response, passwordError
+	}
+	response := model.GetTokenResponse{
+		Success: true,
+		Token:   user.Token,
+	}
+	return &response, nil
 }
 
 // Mutation returns MutationResolver implementation.
